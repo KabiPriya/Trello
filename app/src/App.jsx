1b4c070ca9b54dd98bada40b4bada3a4cb70c1e5
KabiPriya <kabipriya.t@gmail.com>
2023-10-17 23:24:54 +0530

    added column

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,35 +1,106 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useReducer, useState } from "react";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+function taskReducer(state, action) {
+  switch (action.type) {
+    case "ADD_TASK":
+      return [...state, { text: action.text, isEditing: false }];
+    case "EDIT_TASK":
+      return state.map((task, index) => {
+        if (index === action.index) {
+          return { ...task, isEditing: true };
+        }
+        return task;
+      });
+    case "UPDATE_TASK":
+      return state.map((task, index) => {
+        if (index === action.index) {
+          return { ...task, text: action.text, isEditing: false };
+        }
+        return task;
+      });
+    case "DELETE_TASK":
+      return state.filter((_, index) => index !== action.index);
+    default:
+      return state;
+  }
 }
 
-export default App
+function App() {
+  const [todoItems, dispatch] = useReducer(taskReducer, []);
+  const [newTask, setNewTask] = useState("");
+  const [isAddingTask, setIsAddingTask] = useState(false);
+
+  const handleAddTask = () => {
+    if (newTask) {
+      dispatch({ type: "ADD_TASK", text: newTask });
+      setNewTask("");
+      setIsAddingTask(false);
+    }
+  };
+
+  const handleEditTask = (index) => {
+    dispatch({ type: "EDIT_TASK", index });
+  };
+
+  const handleUpdateTask = (index) => {
+    dispatch({ type: "UPDATE_TASK", index, text: newTask });
+  };
+
+  const handleDeleteTask = (index) => {
+    dispatch({ type: "DELETE_TASK", index });
+  };
+
+  return (
+    <div>
+      <div>
+        <h1>Trello</h1>
+      </div>
+      <div className="app">
+        <div className="column">
+          <h2>TO DO</h2>
+          {isAddingTask ? (
+            <div className="task adding">
+              <input
+                type="text"
+                placeholder="New Task"
+                value={newTask}
+                onChange={(e) => setNewTask(e.target.value)}
+              />
+              <button onClick={handleAddTask}>Save</button>
+            </div>
+          ) : (
+            <button className="add-button" onClick={() => setIsAddingTask(true)}>
+              +
+            </button>
+          )}
+          {todoItems.map((task, index) => (
+            <div key={index} className={`task ${task.isEditing ? "editing" : ""}`}>
+              {task.isEditing ? (
+                <input
+                  type="text"
+                  value={newTask}
+                  onChange={(e) => setNewTask(e.target.value)}
+                />
+              ) : (
+                task.text
+              )}
+              {task.isEditing ? (
+                <button onClick={() => handleUpdateTask(index)}>Update</button>
+              ) : (
+                <>
+                  <button onClick={() => handleEditTask(index)}>Edit</button>
+                  <button onClick={() => handleDeleteTask(index)}>Delete</button>
+                </>
+              )}
+            </div>
+          ))}
+        </div>
+        <div className="column">PROCESSING</div>
+        <div className="column">COMPLETED</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
