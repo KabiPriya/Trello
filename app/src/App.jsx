0fdc5d66d5a93054849f257622d400ea7baa6c4c
KabiPriya <kabipriya.t@gmail.com>
2023-10-18 01:14:34 +0530

    added UI

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -21,6 +21,9 @@ function taskReducer(state, action) {
       });
     case "DELETE_TASK":
       return state.filter((_, index) => index !== action.index);
+    case "MOVE_TASK":
+      const movedTask = state[action.index];
+      return state.filter((_, index) => index !== action.index && !movedTask);
     default:
       return state;
   }
@@ -30,6 +33,7 @@ function App() {
   const [todoItems, dispatch] = useReducer(taskReducer, []);
   const [newTask, setNewTask] = useState("");
   const [isAddingTask, setIsAddingTask] = useState(false);
+  const [draggedTask, setDraggedTask] = useState(null);
 
   const handleAddTask = () => {
     if (newTask) {
@@ -51,13 +55,28 @@ function App() {
     dispatch({ type: "DELETE_TASK", index });
   };
 
+  const handleDragStart = (index) => {
+    setDraggedTask(index);
+  };
+
+  const handleDragEnd = () => {
+    setDraggedTask(null);
+  };
+
+  const handleDrop = (targetColumn) => {
+    if (draggedTask !== null) {
+      dispatch({ type: "MOVE_TASK", index: draggedTask, target: targetColumn });
+      setDraggedTask(null);
+    }
+  };
+
   return (
     <div>
       <div className="heading">
         <h1>Trello</h1>
       </div>
       <div className="app">
-        <div className="columntodo">
+        <div className="columntodo" onDrop={() => handleDrop("TO DO")} onDragOver={(e) => e.preventDefault()}>
           <h2>TO DO</h2>
           {isAddingTask ? (
             <div className="task adding">
@@ -70,12 +89,21 @@ function App() {
               <button onClick={handleAddTask}>Save</button>
             </div>
           ) : (
-            <button className="add-button" onClick={() => setIsAddingTask(true)}>
+            <button
+              className="add-button"
+              onClick={() => setIsAddingTask(true)}
+            >
               +
             </button>
           )}
           {todoItems.map((task, index) => (
-            <div key={index} className={`task ${task.isEditing ? "editing" : ""}`}>
+            <div
+              key={index}
+              className={`task ${task.isEditing ? "editing" : ""}`}
+              draggable
+              onDragStart={() => handleDragStart(index)}
+              onDragEnd={handleDragEnd}
+            >
               {task.isEditing ? (
                 <input
                   type="text"
@@ -96,8 +124,20 @@ function App() {
             </div>
           ))}
         </div>
-        <div className="columnprocess"><h2>PROCESSING</h2></div>
-        <div className="columncomplete"><h2>COMPLETED</h2></div>
+        <div
+          className="columnprocess"
+          onDrop={() => handleDrop("PROCESSING")}
+          onDragOver={(e) => e.preventDefault()}
+        >
+          <h2>PROCESSING</h2>
+        </div>
+        <div
+          className="columncomplete"
+          onDrop={() => handleDrop("COMPLETED")}
+          onDragOver={(e) => e.preventDefault()}
+        >
+          <h2>COMPLETED</h2>
+        </div>
       </div>
     </div>
   );
